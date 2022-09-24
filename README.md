ebc51496fdd51c5676b693b82e7b59baec0d23b1
AshishManer <ashishmaner94@gmail.com>
2022-09-24 23:07:30 +0530

    my yhird commit

---
+++ README.md
@@ -0,0 +1,6 @@
+//9rXeEH4klSJ5rGS5
+//mongodb+srv://Ashish:9rXeEH4klSJ5rGS5@cluster0.sl4imij.mongodb.net/?retryWrites=true&w=majority
+
+
+
+//github host:
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:'mongodb+srv://Ashish:9rXeEH4klSJ5rGS5@cluster0.sl4imij.mongodb.net/?retryWrites=true&w=majority',
+    JWT_SECRET:"fgismlpwasdttgcxzsplhgcxz",
+    SENDGRID_API:"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", 
+    EMAIL:"http:localhost:3000"
+}
